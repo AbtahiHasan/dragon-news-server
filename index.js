9a76b1eb5927d1ce1365198d9564bb50ca44d4f4
AbtahiHasan <anikidpersonal@gmail.com>
2023-04-21 21:16:33 +0600

    dragon server done

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const express = require("express")
-// import { Express } from "express"
+import express from "express"
+import cors from "cors";
+
+
+import categoryData from './database/categories.json' assert {type: 'json'}
+import newsData from './database/news.json' assert {type: 'json'}
+
 
 const app = express()
+app.use(cors())
 const port = 3000
+app.get("/", (req, res) => {
+    res.send("server is runing")
+})
+app.get("/categories", (req,res) => {
+    res.send(categoryData)
+})
+
+app.get("/categories/:id", (req,res) => {
+    const id = req.params.id
+    if(id === "0") {
+        return res.send(newsData)
+    }
+    const categores = newsData.filter(news => news.category_id === id)
+    res.send(categores)
+})
+
+
+app.get("/all-news", (req,res) => {
+    res.send(newsData)
+})
+app.get("/news/:id", (req,res) => {
+    const id = req.params.id    
+
+    const newses = newsData.find(news => news._id === id)
+    res.send(newses)
+})
 
 app.listen(port, () => {
     console.log(`this server runing on ${port}`)
